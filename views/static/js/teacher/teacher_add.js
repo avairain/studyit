9419e1cf126f4dc09656f926e6f067dc87e0f91a
rain <753342031@qq.com>
2017-07-01 14:29:09 +0800

    完成了添加功能

--- views/static/js/teacher/teacher_add.js
+++ views/static/js/teacher/teacher_add.js
@@ -6,7 +6,6 @@
 			obj[userInfo[i].split('=')[0]]=userInfo[i].split('=')[1];
 		});
 		console.log(obj);
-		//编辑
 		$.ajax({
 			url:'/api/teacher/view',
 			type:'get',
@@ -15,25 +14,41 @@
 			},
 			success:function(data){
 				console.log(data);
-				var html =template('teacheraddinfo',data.result);
-				$('.teacher-add>form').html(html);
+				//编辑
+
 				if(obj.tc_id){
 					$('.breadcrumb').children('li.active').html('讲师编辑');
 					$('#teacheraddpassword').hide();
 					$("#teacheraddsubmit").html('保 存');
+					var html =template('teacheraddinfo',data.result);
+					$('.teacher-add>form').html(html);
+					$('.teacher-add>form').on('click','#teacheraddsubmit',function(){
+						$('.teacher-add>form').ajaxSubmit({
+							url:'/api/teacher/update',
+							type:'post',
+							success:function(data){
+								console.log(data);
+							}
+						})
+					})
 				}else{
+					// 添加
 					$('.breadcrumb').children('li.active').html('讲师添加');
 					$('#teacheraddpassword').show();
 					$("#teacheraddsubmit").html('添 加');
-				}
-				$('.teacher-add>form').on('click','#teacheraddsubmit',function(){
-					$('.teacher-add>form').ajaxSubmit({
-						url:'/api/teacher/update',
-						success:function(data){
-							console.log(data);
-						}
+					var html =template('teacheraddinfo',{tc_gender:1});
+					$('.teacher-add>form').html(html);
+					$('.teacher-add>form').on('click','#teacheraddsubmit',function(){
+						$('.teacher-add>form').ajaxSubmit({
+							url:'/api/teacher/add',
+							type:'post',
+							success:function(data){
+								console.log(data);
+							}
+						})
 					})
-				})
+				}
+				
 			}
 		})
 		//添加
